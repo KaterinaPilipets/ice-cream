64d92834766b206c53df1d36351ff6f0b3d244c6
KaterinaPilipets <ket.pil.glass@gmail.com>
2022-10-10 03:58:07 +0300

    add skript slider

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,26 +1,12 @@
-// посилання ,які додаються в <head>
-
-//  <!-- Add the slick-theme.css if you want default styling -->
-//   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-//   <!-- Add the slick-theme.css if you want default styling -->
-//   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
-
-// скрипти, які додаються перед закриваючим тегом <body>
-
-//  <!-- slick jquery -->
-//   <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-//   <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-//   <!-- slick js -->
-//   <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
 // сам скрипт для slick
 
-// $('.slick-js').slick({
-//   dots: true,
-//   arrows: false,
-//   infinite: true,
-//   speed: 1000,
-//   slidesToShow: 1,
-//   adaptiveHeight: true,
-//   autoplay: true,
-// });
+$('.slick-js').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+});
