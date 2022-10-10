4cb0129400d9c33baeae9c5361f16dc2b3fd20b6
KaterinaPilipets <109768011+KaterinaPilipets@users.noreply.github.com>
2022-10-10 11:51:47 +0000

    Deploying to gh-pages from @ KaterinaPilipets/ice-cream@636139d544cd28e0d774e435664c35ae621227c9 🚀

---
+++ index.8eb9c675.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[franshise-modal-open]"),closeModalBtn:document.querySelector("[franshise-modal-close]"),modal:document.querySelector("[franshise-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.8eb9c675.js.map
---
+++ index.cb4b931f.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[produkts-modal-open]"),closeModalBtn:document.querySelector("[produkts-modal-close]"),modal:document.querySelector("[produkts-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.cb4b931f.js.map
